109e39b038455de9610ce2e65af2ce9674dece8e
SaranIslam <saranisl.job@gmail.com>
2023-04-04 15:31:28 +0700

    Start
    Introduction

---
+++ index_script.js
@@ -0,0 +1,4 @@
+var lotto = prompt("give me the number")
+var prize = Math.floor(Math.random()*100);
+document.getElementById("input").innerHTML = lotto
+document.getElementById("result").innerHTML = prize
